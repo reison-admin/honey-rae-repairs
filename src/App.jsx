cee25c29a91a8246a12b6d0fdf26803188b083bf
Jeff Ewers <89662718+reison-admin@users.noreply.github.com>
2024-03-15 10:39:16 -0500

    Add tickets list and filter buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,55 @@
+import { useEffect, useState } from "react"
+import { getAllTickets } from "./services/ticketService.js"
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
+  const [allTickets, setAllTickets] = useState([])
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
+  const [filteredTickets, setFilteredTickets] = useState([])
+
+  useEffect(() => {
+    getAllTickets().then(ticketsArray => {
+      setAllTickets(ticketsArray)
+      console.log('Tickets set')
+    })
+  }, []) //empty array runs on initial render of component
+
+  useEffect(() => {
+    if(showEmergencyOnly) {
+      const emergencyTickets = allTickets.filter(ticket => ticket.emergency)
+      setFilteredTickets(emergencyTickets);
+    }
+    else {
+      setFilteredTickets(allTickets);
+    }
+  }, [showEmergencyOnly, allTickets])
+
+return (<div className='tickets-container'>
+    <h2>Tickets</h2>
+    <div>
+      <button className="filter-btn btn-primary" onClick={() => {setShowEmergencyOnly(true)}}>
+        Emergency
+      </button>
+    </div>
+    <div>
+      <button className="filter-btn btn-info" onClick={() => {setShowEmergencyOnly(false)}}>
+        Show All
+      </button>
+    </div>
+    <article className='tickets'>
+      {filteredTickets.map((ticket) => {
+        return (
+          <section className="ticket" key={ticket.id}>
+            <header className="ticket-info">#{ticket.id}</header>
+            <div>{ticket.description}</div>
+            <footer>
+              <div>
+                <div className="ticket-info">emergency</div>
+                <div>{ticket.emergency ? "yes" : "no"}</div>
+              </div>
+            </footer>
+          </section>
+        )
+      })}
+    </article>
+  </div>)
 }
